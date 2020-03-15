f6c7d2b6ae2f4b99842339ee17551d2bf48c6ac7
Pankajan Satkunam <pankajan05@gmail.com>
2020-03-15 10:28:33 +0530

    Add the navigation

--- application/views/Template/header.php
+++ application/views/Template/header.php
@@ -8,4 +8,18 @@
 	</head>
 	<body>
 
+	<nav class="navbar navbar-expand-lg navbar-light bg-dark">
+		<a class="navbar-brand" href="<?php echo base_url(); ?>">Sabi Blogs</a>
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+		<div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+			<div class="navbar-nav">
+				<a class="nav-item nav-link" href="<?php echo base_url(); ?>">Home <span class="sr-only">(current)</span></a>
+				<a class="nav-item nav-link" href="<?php echo base_url(); ?>">Posts</a>
+				<a class="nav-item nav-link" href="<?php echo base_url(); ?>/about">About</a
+			</div>
+		</div>
+	</nav>
+
 
