14dcb2af2d7d5391e1b8578017f8004f4bc56d00
Pankajan Satkunam <pankajan05@gmail.com>
2020-03-15 10:21:27 +0530

    Add the bootstrap, jquery

---
+++ application/views/Template/header.php
@@ -0,0 +1,11 @@
+<html>
+	<head>
+		<title>Sabi Blogs</title>
+		<meta charset="utf-8">
+		<link rel="stylesheet" href="<?php echo base_url('assets/css/bootstrap.min.css');?>">
+		<script src="<?php echo base_url('assets/js/jquery-3.1.0.js'); ?>"></script>
+		<script src="<?php echo base_url('assets/js/bootstrap.min.js'); ?>"></script>
+	</head>
+	<body>
+
+
