e12ae598d2cca45e64756497c07d1075410aaef1
Pankajan Satkunam <pankajan05@gmail.com>
2020-03-15 10:46:32 +0530

    Update footer.php

--- application/views/Template/footer.php
+++ application/views/Template/footer.php
@@ -1,2 +1,8 @@
+
+<footer class="page-footer font-small border-top special-color-dark pt-4">
+	<div class="footer-copyright text-center py-3">© 2020 Copyright:
+		<a href="https://mdbootstrap.com/"> MDBootstrap.com</a>
+	</div>
+</footer>
 </body>
 </html>
