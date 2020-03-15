a2c4927f028c93a1faf4c0740f14752c534c4c4a
Pankajan Satkunam <pankajan05@gmail.com>
2020-03-15 16:48:34 +0530

    Add the image for the posts

--- application/views/posts/index.php
+++ application/views/posts/index.php
@@ -1,11 +1,22 @@
 <div class="container  p-5">
+
 	<h2><?= $title ?></h2>
 	<p>This is my post pages</p>
 
+
 	<?php foreach ($posts as $post) :?>
-	<h3><?php echo $post['Title']; ?></h3>
-	<small>Posted on: <?php echo $post['Date_created']; ?></small>
-	<p><?php echo $post['Summart'] ?></p>
-	<p class="pb-5"><a href="<?php echo site_url('/posts/'. $post['ID']); ?>">Readmore...</a></p>
+	<div class="row p-5">
+		<div class="col-6">
+			<img src="<?php echo base_url().'assets/img/posts/'.$post['Image']; ?>" width="100%">
+		</div>
+		<div class="col-6 pt-5">
+			<h3><?php echo $post['Title']; ?></h3>
+			<div class="h5"><?php echo $post['Category']; ?></div>
+			<small>Posted on: <?php echo $post['Date_created']; ?></small>
+			<p><?php echo $post['Summart'] ?></p>
+			<p class="pb-5"><a href="<?php echo site_url('/posts/'. $post['ID']); ?>">Readmore...</a></p>
+		</div>
+	</div>
 	<?php endforeach; ?>
+
 </div>
--- application/views/posts/view.php
+++ application/views/posts/view.php
@@ -1,8 +1,13 @@
 <div class="container p-5">
-	<h2><?php echo $post['Title']; ?></h2>
-		<div>Created by: <?php echo $post['Author']; ?></div>
-	<small class="pr-5">Posted on: <?php echo $post['Date_created']; ?></small>
-	<small class="pr-5">Updated on: <?php echo $post['Date_updated']; ?></small>
+	
+	<h2 class="text-center"><?php echo $post['Title']; ?></h2>
+		<div class="text-right">Created by: <?php echo $post['Author']; ?><br>
+			<small >Posted on: <?php echo $post['Date_created']; ?></small>
+			<small class="pl-5">Updated on: <?php echo $post['Date_updated']; ?></small>
+		</div>
+
+	<img class="p-5 text-center" src="<?php echo base_url().'assets/img/posts/'.$post['Image']; ?>" width="70%">
+
 	<div class="pt-5 pb-2">Summary: <p><?php echo $post['Summart'] ?></p></div>
 	<p class=""><?php echo $post['Content'] ?></p>
 </div>
