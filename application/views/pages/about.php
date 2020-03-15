e030b873032f093566fb69716450f1d16066e501
Pankajan Satkunam <pankajan05@gmail.com>
2020-03-15 10:52:54 +0530

    add the contact details

--- application/views/pages/about.php
+++ application/views/pages/about.php
@@ -1,1 +1,13 @@
-about
+<div class="container">
+	<h2 class="text-center m-5">Contact Details</h2>
+	<div class="media p-5 m-5" style="border:0.5px  #5a5a5a solid;">
+		<div class="media-body">
+			<h5 class="mt-0">Pankajan Satkunam</h5>
+			<p>I am a software Engineer. I am interested in learning new things.</p>
+			<p> I design this for get experience in design a project in php.I am also interested in gardening. If you like to create like this project then you can contact me. through this number.</p>
+			<p><span href="#" class="font-weight-bold">Telephone Number :</span><span class="ml-5 text-right">+94772958306</span></p>
+			<p><span href="#" class="font-weight-bold">Email Address :</span><span class="ml-5 text-right">pankajan05@gmail.com</span></p>
+			<p><span href="#" class="font-weight-bold">Address: </span><span class="ml-5 p-5 text-right">Earlalai East, Earlalai, Jaffna.</span></p>
+		</div>
+	</div>
+</div>
